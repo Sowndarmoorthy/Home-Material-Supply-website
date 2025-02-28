00cc4776417bd478118df5e63416f499e8894372
sowndar <sowndarmoorthy@gmail.com>
2025-02-28 12:08:55 +0530

    Stage 7: Added search bar and filter

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -3,11 +3,50 @@ import React from 'react'
 
 import CommonSection from '../components/UI/CommonSection'
 import Helmet from '../components/Helmet/Helmet'
+import { Container,Row,Col } from 'reactstrap'
+import "../styles/shop.css"
 
 const Shop = () => {
   return (
     <Helmet title="Shop">
       <CommonSection title="Products" />
+
+      <section>
+        <Container>
+          <Row>
+            <Col lg='3' md='3'>
+              <div className='filter__widget'>
+                <select>
+                 <option>Filter By Category</option>
+                  <option value="Cement">Cement</option>
+                  <option value="Powder">Powder</option>
+                  <option value="Pipe">Pipe</option>
+                  <option value="Sheet">Sheet</option>
+                </select>
+              </div>
+            </Col>
+            <Col lg='3' md='3'>
+              <div className='filter__widget'>
+                  <select>
+                  <option>Sort By</option>
+                    <option value="ascending">Ascending</option>
+                    <option value="descending">Descending</option>
+                  </select>
+                </div>
+            </Col>
+            <Col lg='6' md='6 '>
+              <div className='search__box'>
+                <input type='text' placeholder='Search.......' />
+                <span>
+                  <i className="ri-search-2-line"></i>
+                </span>
+              </div>
+            </Col>
+
+
+          </Row>
+        </Container>
+      </section>
     </Helmet>
   )
 }
